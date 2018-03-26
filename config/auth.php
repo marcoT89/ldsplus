34288305643305985c20e27f51d9e7756c3cc9c7
Marco Tulio de Avila Santos <marcotulio.avila@gmail.com>
2018-03-25 23:17:21 -0300

    [add] Laravel Passport to application

--- config/auth.php
+++ config/auth.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Authentication Defaults
@@ -42,7 +41,7 @@ return [
         ],
 
         'api' => [
-            'driver' => 'token',
+            'driver' => 'passport',
             'provider' => 'users',
         ],
     ],
@@ -67,7 +66,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => App\Models\User::class,
         ],
 
         // 'users' => [
@@ -98,5 +97,4 @@ return [
             'expire' => 60,
         ],
     ],
-
 ];
